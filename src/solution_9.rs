8093b34d399ddb3a1b70f789509b2a46dd20166a
Kenvi Zhu <mrwhite@163.com>
2023-12-21 22:02:30 +0800

    finish 9

--- src/solution_9.rs
+++ src/solution_9.rs
@@ -1,6 +1,38 @@
+struct Solution();
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            false
+        } else {
+            let str = x.to_string();
+            let bytes = str.as_bytes();
+            let len = str.len();
+            let mut start = 0;
+            let mut end = len - 1;
+            while start < end {
+                if bytes[start] != bytes[end] {
+                    return false;
+                }
+                start += 1;
+                end -=1;
+            }
+            return true;
+        }
 
 
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::solution_9::Solution;
+
+    #[test]
+    fn test_basic() {
+        assert_eq!(Solution::is_palindrome(-121), false);
+        assert_eq!(Solution::is_palindrome(121), true);
+        assert_eq!(Solution::is_palindrome(1), true);
+        assert_eq!(Solution::is_palindrome(21), false);
     }
 }
